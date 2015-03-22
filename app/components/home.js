0bb25828890e512ca0c9ff859900b7a8db7c965d
James Hollingworth <jamiehollingworth@gmail.com>
2015-03-22 17:06:00 +0000

    Use Marty.createContainer and react-router 0.13

--- app/components/home.js
+++ app/components/home.js
@@ -5,56 +5,44 @@ var NewRoom = require('./newRoom');
 var RoomsStore = require('../stores/roomsStore');
 var NavigationActionCreators = require('../actions/navigationActionCreators');
 
-class Home extends Marty.Component {
-  constructor(props, context) {
-    super(props, context);
-    this.listenTo = RoomsStore;
-    this.renderRooms = _.bind(this.renderRooms, this);
-    this.navigateToRoom = _.bind(this.navigateToRoom, this);
-  }
-  getState() {
-    return {
-      rooms: RoomsStore.for(this).getAll()
-    };
-  }
+class Home extends React.Component {
   render() {
     return (
       <div className="home">
         <NewRoom />
-        {this.renderRooms()}
+        <ul className="rooms">
+          {_.map(this.props.rooms, (room) => {
+            return (
+              <li className='room'>
+                <a href="javascript:void(0)"
+                   onClick={_.partial(this.navigateToRoom, room.id)}>
+                   {room.name}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     );
   }
-  renderRooms() {
-    var navigateToRoom = this.navigateToRoom;
-    return this.state.rooms.when({
-      pending() {
-        return <div className='pending'>Loading rooms...</div>;
-      },
-      failed(error) {
-        return <div className='error'>Failed to load rooms. {error.message}</div>;
-      },
-      done(rooms) {
-        return (
-          <ul className="rooms">
-            {_.map(rooms, room => {
-              return (
-                <li className='room'>
-                  <a href="#" onClick={_.partial(navigateToRoom, room.id)}>
-                    {room.name}
-                  </a>
-                </li>
-              );
-            })}
-          </ul>
-        );
-      }
-    }, this);
-  }
   navigateToRoom(roomId) {
-    console.log('navigateToRoom')
     NavigationActionCreators.for(this).navigateToRoom(roomId);
   }
 }
 
-module.exports = Home;
+
+
+module.exports = Marty.createContainer(Home, {
+  listenTo: RoomsStore,
+  fetch: {
+    rooms() {
+      return RoomsStore.for(this).getAll();
+    }
+  },
+  pending() {
+    return <div className='pending'>Loading rooms...</div>;
+  },
+  failed(errors) {
+    return <div className='error'>Failed to load rooms. {errors}</div>;
+  }
+});
--- app/components/room.js
+++ app/components/room.js
@@ -5,52 +5,17 @@ var NewMessage = require('./newMessage');
 var RoomsStore = require('../stores/roomsStore');
 var MessagesStore = require('../stores/messagesStore');
 
-class Room extends Marty.Component {
-  constructor(props, context) {
-    super(props, context);
-    this.listenTo = [RoomsStore, MessagesStore];
-  }
-  getState() {
-    return {
-      room: RoomsStore.for(this).getRoom(this.props.id),
-      messages: MessagesStore.for(this).getMessagesForRoom(this.props.id)
-    };
-  }
+class Room extends React.Component {
   render() {
-    var body = this.state.room.when({
-      pending() {
-        return <div className='loading'>Loading...</div>;
-      },
-      failed(error) {
-        return <div className='error'>Failed to load room. {error.message}</div>;
-      },
-      done(room) {
-        return (
-          <div className='room-body'>
-            <h1 className='room-name'>{room.name}</h1>
-            {this.renderMessages()}
-            <NewMessage roomId={room.id} />
-          </div>
-        );
-      }
-    }, this);
+    var room = this.props.room;
+    var messages = _.sortBy(this.props.messages, message => new Date(message.timestamp));
 
-    return <div className='room'>{body}</div>;
-  }
-  renderMessages() {
-    return this.state.messages.when({
-      pending() {
-        return <div className='messages-loading'>Loading messages...</div>
-      },
-      failed(error) {
-        return <div className='messages-error'>Failed to load messages. {error.message}</div>
-      },
-      done(messages) {
-        messages = _.sortBy(messages, message => new Date(message.timestamp));
-
-        return (
+    return (
+      <div className='room'>
+        <div className='room-body'>
+          <h1 className='room-name'>{room.name}</h1>
           <ul className='messages'>
-            {_.map(messages, message => {
+            {_.map(messages, (message) => {
               return (
                 <li className='message'>
                   <div className='message-text'>
@@ -60,10 +25,27 @@ class Room extends Marty.Component {
               );
             })}
           </ul>
-        );
-      }
-    });
+          <NewMessage roomId={room.id} />
+        </div>
+      </div>
+    );
   }
 }
 
-module.exports = Room;
+module.exports = Marty.createContainer(Room, {
+  listenTo: [RoomsStore, MessagesStore],
+  fetch: {
+    room() {
+      return RoomsStore.for(this).getRoom(this.props.id)
+    },
+    messages() {
+      return MessagesStore.for(this).getMessagesForRoom(this.props.id)
+    }
+  },
+  pending() {
+    return <div className='loading'>Loading...</div>;
+  },
+  failed(errors) {
+    return <div className='error'>Failed to load room. {errors}</div>;
+  }
+});
